1bd768fb2e2f74aff662a02aad02a90f7a376252
Bartosz Kowalczyk <bartoszkowalczyk@wp.pl>
2022-12-28 16:58:40 +0100

    loading page add

---
+++ scripts/loader.js
@@ -0,0 +1,10 @@
+function changeVisibility() {
+    setTimeout(() => {
+        document.getElementById('container').classList.add('active');
+        document.getElementById('loader').classList.remove('active');
+        document.getElementById('circle').classList.remove('active');
+    }, 2000);
+
+};
+
+window.addEventListener('load', changeVisibility);
