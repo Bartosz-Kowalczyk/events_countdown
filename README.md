91ec9bf5334c059dbda7ccd13b79b631e6d84b86
Bartosz Kowalczyk <bartoszkowalczyk@wp.pl>
2022-12-28 19:58:33 +0100

    README info add

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# events_countdown
+# Events countdown
+
+## About
+
+This is a simple app to countdown the time before the event.
+
+Example: https://bartkowalczyk.com/events_countdown
+
+## Instalation
+
+Just copy the code to your website.
+It's made with HTML5, CSS3 and vanilla JS only.
+No framework and/or libraries needed.
+
+## Customization
+
+### Appearance
+
+If you need to adjust the form to your website just change the value of those variables in **style.css**:
+
+```CSS
+:root {
+    --font-family: 'Varela Round', sans-serif;
+    --background-color: #457B9D;
+    --font-color: #ffffff;
+    --border-radius: 5px;
+    --app-backgroud-color: #0c2b44a6;
+    --countdown-title-backgroud: #E63946;
+    --countdown-title-font-color: #F1FAEE;
+    --countdown-backgroud: #f8fcf7;
+    --input-background: #F1FAEE;
+    --input-border-color: #1D3557;
+    --text-shadow: 0px 0px 9px rgba(0,0,0,0.37);
+    --button-background: #e63947d7;
+}
+```
--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,51 +1,50 @@
 class Countdown {
+  constructor({ title, date, id }) {
+    this.title = title;
+    this.date = date;
+    this.timestamp = date.getTime();
+    this.id = id;
+    this.isFinished = false;
 
-    constructor({title, date, id}) {
-        this.title = title;
-        this.date = date;
-        this.timestamp = date.getTime();
-        this.id = id;
-        this.isFinished = false;
+    this.container = document.getElementById(id);
+    this.daysElement = this.container.querySelector(".days");
+    this.hoursElement = this.container.querySelector(".hours");
+    this.minutesElement = this.container.querySelector(".minutes");
+    this.secondsElement = this.container.querySelector(".seconds");
 
-        this.container = document.getElementById(id);
-        this.daysElement = this.container.querySelector('.days');
-        this.hoursElement = this.container.querySelector('.hours');
-        this.minutesElement = this.container.querySelector('.minutes');
-        this.secondsElement = this.container.querySelector('.seconds');
+    this.deleteButton = this.container.querySelector(".countdown-delete");
+  }
 
-        this.deleteButton = this.container.querySelector('.countdown-delete');
-    }
+  addDeleteListener = (callback) => {
+    this.deleteButton.addEventListener("click", callback);
+  };
 
-    addDeleteListener = (callback) => {
-        this.deleteButton.addEventListener('click', callback);
-    }
+  updateCountdownHtml = ({ days, hours, minutes, seconds }) => {
+    this.daysElement.textContent = days;
+    this.hoursElement.textContent = hours;
+    this.minutesElement.textContent = minutes;
+    this.secondsElement.textContent = seconds;
+  };
 
-    updateCountdownHtml = ({days, hours, minutes, seconds}) => {
-        this.daysElement.textContent = days;
-        this.hoursElement.textContent = hours;
-        this.minutesElement.textContent = minutes;
-        this.secondsElement.textContent = seconds;
-    }
+  countdownFinished = () => {
+    if (this.isFinished) return;
+    this.isFinished = true;
 
-    countdownFinished = () => {
-        if (this.isFinished) return;
-        this.isFinished = true;
+    this.container.classList.add("countdown-finished");
+    this.container.querySelector(".timer").classList.add("hide");
+    this.container
+      .querySelector(".countdown-finished-info")
+      .classList.add("show");
+    console.log(`Countdown ${this.id} finished.`);
+  };
 
-        this.container.classList.add('countdown-finished');
-        this.container.querySelector('.timer').classList.add('hide');
-        this.container.querySelector('.countdown-finished-info').classList.add('show');
-        console.log(`Countdown ${this.id} finished.`)
-    }
+  getId = () => {
+    return this.id;
+  };
 
-    getId = () => {
-        return this.id;
-    }
+  getContainer = () => {
+    return this.container;
+  };
 
-    getContainer = () => {
-        return this.container;
-    }
-
-    destroyCountdown = () => {
-
-    }
+  destroyCountdown = () => {};
 }
